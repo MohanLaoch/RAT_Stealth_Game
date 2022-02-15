8a04e62bfb768fe1a985ea4f68f2cd5e70264e36
James Mohan <c20305011@mytudublin.ie>
2022-02-15 22:26:27 +0000

    Made the doors work

--- RAT Stealth Game/Assets/Script/DoorOpen.cs
+++ RAT Stealth Game/Assets/Script/DoorOpen.cs
@@ -9,11 +9,19 @@ public class DoorOpen : MonoBehaviour
     public Sprite doorOpen;
     public Sprite doorClosed;
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             spriteRenderer.sprite = doorOpen;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            spriteRenderer.sprite = doorClosed;
         }
     }
 
